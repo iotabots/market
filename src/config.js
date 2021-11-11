14b7a7afd4a7d63001954482275efd55bc747c16
huhn511 <sehe89@gmail.com>
2021-11-11 18:56:02 +0100

    update contract addresses

--- src/config.js
+++ src/config.js
@@ -1,2 +1,2 @@
-export const nftmarketaddress = "0xB5Efe7508282597dBa0721f19Ef3994545189Dc6"
-export const nftaddress = "0xe44459265B4661CDDe4acE3E661253DA3f72Eab7"
+export const nftmarketaddress = "0xD36a66546bE919c35B10605f999f4773CdBfBe91"
+export const nftaddress = "0x9f67BC379AbE73d31559D38E31f8584E96c5662d"
