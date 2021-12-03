5d6e5939e53aba64fd180f4a891f7165b26771bb
huhn511 <sehe89@gmail.com>
2021-12-03 18:27:43 +0100

    update sc addresses

--- src/config.js
+++ src/config.js
@@ -1,2 +1,2 @@
-export const nftmarketaddress = "0x8b2649424048e559C36b25386a0eec878E82d62d"
-export const nftaddress = "0xB4E24C6682aefd40A8016b1c481707328300a910"
+export const nftmarketaddress = "0x3a3c0D4BDAB6d0e9715Fa2eAA852af3038Bec342"
+export const nftaddress = "0x60c61c08F5E4A3559f78716050e77558BE728650"
