ce6be31f9b4dffdae762a9ef6f64f5dc61fb9e0b
huhn511 <sehe89@gmail.com>
2021-12-09 18:48:11 +0100

    add new SC addresses

--- src/config.js
+++ src/config.js
@@ -1,2 +1,2 @@
-export const nftmarketaddress = "0x3a3c0D4BDAB6d0e9715Fa2eAA852af3038Bec342"
-export const nftaddress = "0x60c61c08F5E4A3559f78716050e77558BE728650"
+export const nftmarketaddress = "0xF25eC7d0342baa587A33e402d007412E1AcC8907"
+export const nftaddress = "0x97CCF6c3167a23fA147Ed96659E87498cf860983"
